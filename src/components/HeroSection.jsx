55dbe0f5f479e1a45af65fc41b47a9f1c913da33
Amna Rehan <amnarehan.2665@gmail.com>
2026-01-07 13:18:01 +0500

    updates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -21,7 +21,7 @@ export const HeroSection = () => {
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-            Computer Science Student | Full Stack Web Developer
+            Computer Science Student 
           </p>
 
           <div className="pt-4 opacity-0 animate-fade-in-delay-4">
